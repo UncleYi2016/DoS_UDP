ad81ed86166b5cfd7c5bf2a78ca39c3d56e0c5ac
i330014059 <i330014059@UIC.LOCAL>
2016-04-22 15:08:50 +0800

    fix version

--- Dos_UDP.cpp
+++ Dos_UDP.cpp
@@ -20,91 +20,82 @@ char flush[100];
 
 int main(int argc, char **argv) {
 
-    char szBuff[200];
-    char tt[4096] = { 'g' };
-    int msg_len;
-    int count = 0;
-    //int addr_len;
-    struct sockaddr_in server_addr;
-    struct hostent *hp;
-    SOCKET connect_sock;
-    WSADATA wsaData;
+	char szBuff[200];
+	char tt[4096] = { 'g' };
+	int msg_len;
+	int count = 0;
+	//int addr_len;
+	struct sockaddr_in server_addr;
+	struct hostent *hp;
+	SOCKET connect_sock;
+	WSADATA wsaData;
 
-    char			server_name[30];
-    char            cmd[100];
-    unsigned short	port = DEFAULT_PORT;
-    unsigned int	addr;
+	char			server_name[30];
+	char            cmd[100];
+	unsigned short	port = DEFAULT_PORT;
+	unsigned int	addr;
 
-    scanf("%s", &server_name);
-    scanf("%d", &port);
+	scanf("%s", &server_name);
+	scanf("%d", &port);
 
-    if (WSAStartup(0x202, &wsaData) == SOCKET_ERROR) {
-        // stderr: standard error are printed to the screen.
-        fprintf(stderr, "WSAStartup failed with error %d\n", WSAGetLastError());
-        //WSACleanup function terminates use of the Windows Sockets DLL. 
-        WSACleanup();
-        return -1;
-    }
+	if (WSAStartup(0x202, &wsaData) == SOCKET_ERROR) {
+		// stderr: standard error are printed to the screen.
+		fprintf(stderr, "WSAStartup failed with error %d\n", WSAGetLastError());
+		//WSACleanup function terminates use of the Windows Sockets DLL. 
+		WSACleanup();
+		return -1;
+	}
 
-    if (isalpha(server_name[0]))
-        hp = gethostbyname(server_name);
-    else {
-        addr = inet_addr(server_name);
-        hp = gethostbyaddr((char*)&addr, 4, AF_INET);
-    }
+	if (isalpha(server_name[0]))
+		hp = gethostbyname(server_name);
+	else {
+		addr = inet_addr(server_name);
+		hp = gethostbyaddr((char*)&addr, 4, AF_INET);
+	}
 
-    if (hp == NULL)
-    {
-        fprintf(stderr, "Cannot resolve address: %d\n", WSAGetLastError());
-        WSACleanup();
-        return -1;
-    }
+	if (hp == NULL)
+	{
+		fprintf(stderr, "Cannot resolve address: %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
 
-    //copy the resolved information into the sockaddr_in structure
-    memset(&server_addr, 0, sizeof(server_addr));
-    memcpy(&(server_addr.sin_addr), hp->h_addr, hp->h_length);
-    server_addr.sin_family = hp->h_addrtype;
-    server_addr.sin_port = htons(port);
-
-
-    connect_sock = socket(AF_INET, SOCK_DGRAM, 0);	//TCp socket
+	//copy the resolved information into the sockaddr_in structure
+	memset(&server_addr, 0, sizeof(server_addr));
+	memcpy(&(server_addr.sin_addr), hp->h_addr, hp->h_length);
+	server_addr.sin_family = hp->h_addrtype;
+	server_addr.sin_port = htons(port);
 
 
-    if (connect_sock == INVALID_SOCKET) {
-        fprintf(stderr, "socket() failed with error %d\n", WSAGetLastError());
-        WSACleanup();
-        return -1;
-    }
+	connect_sock = socket(AF_INET, SOCK_DGRAM, 0);	//TCp socket
 
-    printf("Client connecting to: %s\n", hp->h_name);
 
-    if (connect(connect_sock, (struct sockaddr *)&server_addr, sizeof(server_addr))
-        == SOCKET_ERROR) {
-        fprintf(stderr, "connect() failed with error %d\n", WSAGetLastError());
-        WSACleanup();
-        return -1;
-    }
+	if (connect_sock == INVALID_SOCKET) {
+		fprintf(stderr, "socket() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
 
-    while (1) {
-        scanf("%s", &cmd);
-        msg_len = send(connect_sock, cmd, sizeof(cmd), 0);
-        printf("send %d\n", ++count);
-        if (msg_len == SOCKET_ERROR) {
-            fprintf(stderr, "send() failed with error %d\n", WSAGetLastError());
-            WSACleanup();
-            return -1;
-        }
-        msg_len = recv(connect_sock, szBuff, sizeof(szBuff), 0);
-        if (msg_len == SOCKET_ERROR) {
-            fprintf(stderr, "recv() failed with error %d\n", WSAGetLastError());
-            WSACleanup();
-            return -1;
-        }
-        printf("%s", szBuff);
+	printf("Client connecting to: %s\n", hp->h_name);
 
-    }
-    
-    closesocket(connect_sock);
-    WSACleanup();
-    system("Pause");
+	if (connect(connect_sock, (struct sockaddr *)&server_addr, sizeof(server_addr))
+		== SOCKET_ERROR) {
+		fprintf(stderr, "connect() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	while (1) {
+		msg_len = send(connect_sock, tt, sizeof(tt), 0);
+		printf("send %d\n", ++count);
+		if (msg_len == SOCKET_ERROR) {
+			fprintf(stderr, "send() failed with error %d\n", WSAGetLastError());
+			WSACleanup();
+			return -1;
+		}
+	}
+
+	closesocket(connect_sock);
+	WSACleanup();
+	system("Pause");
 }
